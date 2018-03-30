635d0a4a4ef03a9e4826f7acbb2b5a19a077abce
Jenkins <hshengyan@ceibs.edu>
2018-03-30 20:05:37 +0800

    调整ssh的keepalive，分拆代码

---
+++ ssh/exec.go
@@ -0,0 +1,140 @@
+package ssh
+
+import (
+	"errors"
+	"os"
+	"time"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+//一个Session只能执行一次，获取ssh执行命令的结果
+func (this *SSH) Exec(cmd string) (string, error) {
+
+	sess, err := this.c.NewSession()
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		_ = sess.Close()
+	}()
+
+	c, err := sess.CombinedOutput(cmd)
+
+	return string(c), err
+}
+
+//一个Session只能执行一次，直接把结果输出到终端
+//不允许超过3s
+func (this *SSH) ExecWithPty(cmd string, timeout time.Duration) error {
+
+	fd := 0
+	if terminal.IsTerminal(fd) {
+		termWidth, termHeight, err := terminal.GetSize(fd)
+		if err != nil {
+			return err
+		}
+
+		oldState, err := terminal.MakeRaw(fd)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			_ = terminal.Restore(fd, oldState)
+		}()
+
+		sess, err := this.c.NewSession()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			_ = sess.Close()
+		}()
+
+		//如果没有stdin，top之类的命令无法操作
+		// sess.Stdin = os.Stdin
+		sess.Stdout = os.Stdout
+		sess.Stderr = os.Stderr
+
+		modes := ssh.TerminalModes{
+			ssh.ECHO:          0,
+			ssh.TTY_OP_ISPEED: 14400,
+			ssh.TTY_OP_OSPEED: 14400,
+		}
+
+		if err := sess.RequestPty("xterm", termHeight, termWidth, modes); err != nil {
+			return err
+		}
+
+		// return sess.Run(cmd)
+
+		_ = sess.Start(cmd)
+		done := false
+		if timeout > 0 {
+			go func(sess *ssh.Session) {
+				select {
+				case <-time.After(timeout * time.Second):
+					if !done {
+						_ = sess.Close()
+					}
+				}
+
+			}(sess)
+		}
+		err = sess.Wait()
+		done = true
+
+		return err
+	} else {
+		return errors.New("no terminal")
+	}
+}
+
+//一个Session只能执行一次，进入ssh模式
+func (this *SSH) Shell() error {
+
+	fd := 0
+	if terminal.IsTerminal(fd) {
+		termWidth, termHeight, err := terminal.GetSize(fd)
+		if err != nil {
+			return err
+		}
+
+		oldState, err := terminal.MakeRaw(fd)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			_ = terminal.Restore(fd, oldState)
+		}()
+
+		sess, err := this.c.NewSession()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			_ = sess.Close()
+		}()
+
+		sess.Stdin = os.Stdin
+		sess.Stdout = os.Stdout
+		sess.Stderr = os.Stderr
+
+		modes := ssh.TerminalModes{
+			ssh.ECHO: 1,
+		}
+
+		if err := sess.RequestPty("xterm", termHeight, termWidth, modes); err != nil {
+			return err
+		}
+
+		if err := sess.Shell(); err != nil {
+			return err
+		}
+
+		return sess.Wait()
+	} else {
+		return errors.New("no terminal")
+	}
+}
--- ssh/ssh.go
+++ ssh/ssh.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"golang.org/x/crypto/ssh"
-	"golang.org/x/crypto/ssh/terminal"
 )
 
 //SSHConfig ..
@@ -60,7 +59,7 @@ func (this *SSH) Dial() (err error) {
 	this.c, err = ssh.Dial("tcp", this.config.Addr, this.getSshClientConfig())
 
 	if err == nil {
-		this.Keepalive()
+		this.keepalive()
 	}
 
 	return
@@ -91,7 +90,7 @@ func (this *SSH) DialRemote(sshConfig SSHConfig) (ins *SSH, err error) {
 	ins.c = ssh.NewClient(conn, nc, req)
 
 	if err == nil {
-		ins.Keepalive()
+		ins.keepalive()
 	}
 
 	return
@@ -162,137 +161,7 @@ func (this *SSH) getAuth() ssh.AuthMethod {
 	return ssh.PublicKeys(signer)
 }
 
-//一个Session只能执行一次，获取ssh执行命令的结果
-func (this *SSH) Exec(cmd string) (string, error) {
-
-	sess, err := this.c.NewSession()
-	if err != nil {
-		return "", err
-	}
-	defer func() {
-		_ = sess.Close()
-	}()
-
-	c, err := sess.CombinedOutput(cmd)
-
-	return string(c), err
-}
-
-//一个Session只能执行一次，直接把结果输出到终端
-//不允许超过3s
-func (this *SSH) ExecWithPty(cmd string, timeout time.Duration) error {
-
-	fd := 0
-	if terminal.IsTerminal(fd) {
-		termWidth, termHeight, err := terminal.GetSize(fd)
-		if err != nil {
-			return err
-		}
-
-		oldState, err := terminal.MakeRaw(fd)
-		if err != nil {
-			return err
-		}
-		defer func() {
-			_ = terminal.Restore(fd, oldState)
-		}()
-
-		sess, err := this.c.NewSession()
-		if err != nil {
-			return err
-		}
-		defer func() {
-			_ = sess.Close()
-		}()
-
-		//如果没有stdin，top之类的命令无法操作
-		// sess.Stdin = os.Stdin
-		sess.Stdout = os.Stdout
-		sess.Stderr = os.Stderr
-
-		modes := ssh.TerminalModes{
-			ssh.ECHO:          0,
-			ssh.TTY_OP_ISPEED: 14400,
-			ssh.TTY_OP_OSPEED: 14400,
-		}
-
-		if err := sess.RequestPty("xterm", termHeight, termWidth, modes); err != nil {
-			return err
-		}
-
-		// return sess.Run(cmd)
-
-		_ = sess.Start(cmd)
-		done := false
-		if timeout > 0 {
-			go func(sess *ssh.Session) {
-				select {
-				case <-time.After(timeout * time.Second):
-					if !done {
-						_ = sess.Close()
-					}
-				}
-
-			}(sess)
-		}
-		err = sess.Wait()
-		done = true
-
-		return err
-	} else {
-		return errors.New("no terminal")
-	}
-}
-
-//一个Session只能执行一次，进入ssh模式
-func (this *SSH) Shell() error {
-
-	fd := 0
-	if terminal.IsTerminal(fd) {
-		termWidth, termHeight, err := terminal.GetSize(fd)
-		if err != nil {
-			return err
-		}
-
-		oldState, err := terminal.MakeRaw(fd)
-		if err != nil {
-			return err
-		}
-		defer func() {
-			_ = terminal.Restore(fd, oldState)
-		}()
-
-		sess, err := this.c.NewSession()
-		if err != nil {
-			return err
-		}
-		defer func() {
-			_ = sess.Close()
-		}()
-
-		sess.Stdin = os.Stdin
-		sess.Stdout = os.Stdout
-		sess.Stderr = os.Stderr
-
-		modes := ssh.TerminalModes{
-			ssh.ECHO: 1,
-		}
-
-		if err := sess.RequestPty("xterm", termHeight, termWidth, modes); err != nil {
-			return err
-		}
-
-		if err := sess.Shell(); err != nil {
-			return err
-		}
-
-		return sess.Wait()
-	} else {
-		return errors.New("no terminal")
-	}
-}
-
-func (this *SSH) Keepalive() {
+func (this *SSH) keepalive() {
 	if this.c == nil {
 		return
 	}
@@ -306,19 +175,14 @@ func (this *SSH) Keepalive() {
 				return
 			case <-t.C:
 				go func() {
-					_ = this.keepalive()
+					_ = this.Keepalive()
 				}()
 			}
 		}
 	}()
 }
 
-func (this *SSH) keepalive() (err error) {
-	defer func() {
-		if e := recover(); e != nil {
-			err = errors.New("keepalive error")
-		}
-	}()
+func (this *SSH) Keepalive() (err error) {
 	if this.c == nil {
 		return errors.New("keepalive no ins")
 	}
