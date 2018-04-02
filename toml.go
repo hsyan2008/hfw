a9f8532ff9af348dabc77450ac712885994fa780
Jenkins <hshengyan@ceibs.edu>
2018-04-02 17:10:28 +0800

    增加toml格式读取的通用方法

--- toml.go
+++ toml.go
@@ -2,9 +2,11 @@ package hfw
 
 import (
 	"bytes"
+	"errors"
 	"io/ioutil"
 
 	"github.com/BurntSushi/toml"
+	"github.com/hsyan2008/hfw2/common"
 )
 
 func TomlSave(file string, c interface{}) error {
@@ -15,4 +17,13 @@ func TomlSave(file string, c interface{}) error {
 		return err
 	}
 	return ioutil.WriteFile(file, buf.Bytes(), 0644)
+}
+
+func TomlLoad(file string, c interface{}) (err error) {
+	if common.IsExist(file) {
+		_, err := toml.DecodeFile(file, c)
+		return err
+	}
+
+	return errors.New(file + " not exist")
 }
