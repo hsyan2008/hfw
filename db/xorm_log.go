015b4049c7ac21ec51d970a408541c0ab8a62eab
Harvey <shengyan.huang@xforwardai.com>
2019-08-27 13:03:46 +0800

    add fix xorg logger

--- db/xorm_log.go
+++ db/xorm_log.go
@@ -12,35 +12,35 @@ type xormLog struct {
 }
 
 func (this *xormLog) Debug(v ...interface{}) {
-	logger.Output(4, "DEBUG", logger.GetPrefix(), v...)
+	logger.Output(3, "DEBUG", logger.GetPrefix(), v...)
 }
 
 func (this *xormLog) Debugf(format string, v ...interface{}) {
-	logger.Output(4, "DEBUG", logger.GetPrefix(), fmt.Sprintf(format, v...))
+	logger.Output(3, "DEBUG", logger.GetPrefix(), fmt.Sprintf(format, v...))
 }
 
 func (this *xormLog) Info(v ...interface{}) {
-	logger.Output(4, "INFO", logger.GetPrefix(), v...)
+	logger.Output(3, "INFO", logger.GetPrefix(), v...)
 }
 
 func (this *xormLog) Infof(format string, v ...interface{}) {
-	logger.Output(4, "INFO", logger.GetPrefix(), fmt.Sprintf(format, v...))
+	logger.Output(3, "INFO", logger.GetPrefix(), fmt.Sprintf(format, v...))
 }
 
 func (this *xormLog) Warn(v ...interface{}) {
-	logger.Output(4, "WARN", logger.GetPrefix(), v...)
+	logger.Output(3, "WARN", logger.GetPrefix(), v...)
 }
 
 func (this *xormLog) Warnf(format string, v ...interface{}) {
-	logger.Output(4, "WARN", logger.GetPrefix(), fmt.Sprintf(format, v...))
+	logger.Output(3, "WARN", logger.GetPrefix(), fmt.Sprintf(format, v...))
 }
 
 func (this *xormLog) Error(v ...interface{}) {
-	logger.Output(4, "ERROR", logger.GetPrefix(), v...)
+	logger.Output(3, "ERROR", logger.GetPrefix(), v...)
 }
 
 func (this *xormLog) Errorf(format string, v ...interface{}) {
-	logger.Output(4, "ERROR", logger.GetPrefix(), fmt.Sprintf(format, v...))
+	logger.Output(3, "ERROR", logger.GetPrefix(), fmt.Sprintf(format, v...))
 }
 
 func (this *xormLog) Level() core.LogLevel {
