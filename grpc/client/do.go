679f1dda5dd80e3cfd44b3714ac6015458fdef82
Harvey <shengyan.huang@xforwardai.com>
2019-09-06 17:03:24 +0800

    fix grpc do

--- grpc/client/do.go
+++ grpc/client/do.go
@@ -38,7 +38,7 @@ FOR:
 	for i := 0; i < common.Min(retry, len(c.Addresses)+1); i++ {
 		select {
 		case <-ctx.Done():
-			break FOR
+			return nil, ctx.Err()
 		default:
 			if c.IsAuth {
 				conn, err = GetConnWithAuth(signal.GetSignalContext().Ctx, c, "")
