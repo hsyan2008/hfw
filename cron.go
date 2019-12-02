5b428d2bba871aabf9e7aad38a88bc7a7bdadbc5
Harvey <shengyan.huang@xforwardai.com>
2019-12-02 13:28:07 +0800

    add AddWrapCron

--- cron.go
+++ cron.go
@@ -1,6 +1,10 @@
 package hfw
 
 import (
+	"time"
+
+	"github.com/hsyan2008/hfw/common"
+	"github.com/hsyan2008/hfw/signal"
 	cron "github.com/robfig/cron/v3"
 )
 
@@ -17,4 +21,25 @@ func AddCron(spec string, cmd func()) (cron.EntryID, error) {
 
 func StopCron() {
 	crontab.Stop()
+}
+
+func AddWrapCron(spec string, cmd func(httpCtx *HTTPContext) error) (cron.EntryID, error) {
+	return AddCron(spec, func() {
+		httpCtx := NewHTTPContext()
+		signal.GetSignalContext().WgAdd()
+		defer func(now time.Time) {
+			if err := recover(); err != nil {
+				if err != ErrStopRun {
+					httpCtx.Warn(err, string(common.GetStack()))
+				}
+			}
+			signal.GetSignalContext().WgDone()
+			httpCtx.Infof("CostTime: %s", time.Since(now))
+		}(time.Now())
+
+		err := cmd(httpCtx)
+		if err != nil {
+			httpCtx.Warn(err)
+		}
+	})
 }
