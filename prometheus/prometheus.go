00cc373e8fabf90bef817be7812b4ca705801a74
Harvey <hsyan2008@qq.com>
2021-04-09 18:53:40 +0800

    fix prometheus

--- prometheus/prometheus.go
+++ prometheus/prometheus.go
@@ -14,6 +14,7 @@ var (
 	conf             configs.PrometheusConfig
 	requestsTotal    *prometheus.CounterVec
 	requestsCosttime *prometheus.SummaryVec
+	float64Duration  = float64(time.Millisecond)
 )
 
 func Init(c configs.PrometheusConfig) {
@@ -24,14 +25,16 @@ func Init(c configs.PrometheusConfig) {
 	requestsTotal = promauto.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: c.RequestsTotal,
-			Help: strings.ReplaceAll(c.RequestsTotal, "_", ""),
+			Help: strings.ReplaceAll(c.RequestsTotal, "_", " "),
 		},
 		[]string{"app", "host", "path", "method"},
 	)
 	requestsCosttime = promauto.NewSummaryVec(
 		prometheus.SummaryOpts{
-			Name: c.RequestsCosttime,
-			Help: strings.ReplaceAll(c.RequestsCosttime, "_", ""),
+			Name:       c.RequestsCosttime,
+			Help:       strings.ReplaceAll(c.RequestsCosttime, "_", " "),
+			Objectives: map[float64]float64{0.5: 0.05, 0.75: 0.05, 0.9: 0.01, 0.99: 0.001},
+			// MaxAge:     time.Minute,
 		},
 		[]string{"app", "host", "path", "method"},
 	)
@@ -54,5 +57,5 @@ func RequestsCosttime(path, method string, duration time.Duration) {
 	requestsCosttime.WithLabelValues(common.GetAppName(),
 		common.GetHostName(),
 		path,
-		method).Observe(float64(duration / time.Millisecond))
+		method).Observe(float64(duration) / float64Duration)
 }
